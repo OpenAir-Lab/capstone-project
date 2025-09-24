88eb7d646c1567acefc4acb6c45d64c93e1d94b2
ericmutton <ericmutton@protonmail.com>
2025-09-24 14:20:41 -0500

    feat: begin transceiver driver and capture command strobes #12

---
+++ Firmware/system-testbed-fw/lib/cc1200/cc1200.cpp
@@ -0,0 +1,131 @@
+#include <cc1200.h>
+// 
+// Configure the CC120X
+// 
+extern cc1200_config_t cc1200;
+
+int cc1200_init(cc1200_config_t &cc1200) {
+    // Initialize SPI
+    cc1200.spi->setFrequency(10000000); // 10 MHz
+    cc1200.spi->begin(cc1200.sck);
+    // 100 ns delay between consecutive data bytes must be added
+    // during burst write access to the configuration registers.
+    return 0;
+}
+// Section 3.1.1 4-Wire Serial Configuration and Data Interface
+// SPI interface transfers are MSB-first.
+// CSn pin must be kept low during transfers on the SPI bus.
+// Header byte with R/Wn bit, Burst access B bit, and 6-bit address.
+
+// Table 3: SPI Access Types
+int cc1200_single_register_access() {
+    return 0;
+}
+
+int cc1200_burst_register_access() {
+    return 0;
+}
+
+
+int cc1200_command_strobe_access(cc1200_command_strobe_t strobe) {
+    // The chip status byte is returned on the MISO line 
+    // when a command strobe is sent on the MOSI line.
+    return 0;
+}
+
+// Section 3.1.2 Chip Status Byte
+// Status byte is sent on the MISO pin each time a header byte is transmitted on the MOSI pin.
+
+// 
+// Program CC120X into different modes (RX, TX, SLEEP, IDLE, etc)
+//
+
+// RXOFF_MODE=01 or TXOFF_MODE=01 means return to SFSTXON.
+
+// Entered on 
+// SRX                      from FSSTXON
+// SRX/WOR                  from FSCAL, 
+// SRX     or TXOFF_MODE=11 from STX
+int cc1200_receive_mode() {
+    cc1200_command_strobe_access(SRX);
+    return 0;
+}
+// Left on 
+//            RXOFF_MODE=00 to FSCAL,
+// SFSTXON or RXOFF_MODE=01 to SFSTXON,
+// STX     or RXOFF_MODE=10 to STX,
+// Data Buffer Error        to RX_FIFO_ERROR
+
+// Entered on STX or RXOFF_MODE=10, 
+int cc1200_transmit_mode() {
+    cc1200_command_strobe_access(STX);
+    return 0;
+}
+// Left on
+//            TXOFF_MODE=00 to FSCAL,
+//            TXOFF_MODE=01 to FSSTXON,
+// SRX     or TXOFF_MODE=11 to SRX,
+// Data Buffer Error        to TX_FIFO_ERROR 
+
+// Entered on
+// SIDLE                    
+//                          from FSCAL
+// SFRX                     from RX_FIFO_ERROR
+// SFTX                     from TX_FIFO_ERROR
+int cc1200_idle_mode() {
+    cc1200_command_strobe_access(SNOP);
+    return 0;
+}
+// Left on
+// SFSTXON/SRX/STX/WOR      to SFSTXON
+// SCAL                     to FSCAL
+// SPWD or WOR              to SLEEP
+
+// Entered on
+// SPWD or WOR              from IDLE
+int cc1200_sleep_mode() {
+    cc1200_command_strobe_access(SNOP);
+    return 0;
+}
+// Left on CSn=0
+//
+// Read and write buffered data (RX FIFO and TX FIFO)
+//
+
+
+int cc1200_standard_FIFO_access() {
+    return 0;
+}
+
+int cc1200_receive_FIFO_byte() {
+    // See Section 3.2.3
+    return 0;
+}
+
+int cc1200_transmit_FIFO_byte() {
+    return 0;
+}
+// SERIAL_STATUS.SPI_DIRECT_ACCESS_CFG
+// Configures which memory to access when using direct memory access
+int cc1200_direct_FIFO_access() {
+    // SERIAL_STATUS.SPI_DIRECT_ACCESS_CFG = 0
+    return 0;
+}
+
+// FEC Workspace or AES Command Workspace (128 bytes free area).
+// (PKT_CFG1.FEC_EN = 1) (free space otherwise)
+int cc1200_direct_FEC_access() {
+    // SERIAL_STATUS.SPI_DIRECT_ACCESS_CFG = 1
+    // (Address < 0x80)
+    return 0;
+}
+
+int cc1200_direct_AES_access() {
+    // SERIAL_STATUS.SPI_DIRECT_ACCESS_CFG = 1
+    
+    // (0x80 < Address < 0xFF)
+    return 0;
+}
+//
+// Read status information
+//
---
+++ Firmware/system-testbed-fw/lib/cc1200/cc1200.h
@@ -0,0 +1,36 @@
+#include <SPI.h>
+
+// VSPI matrix 
+// SCK 5 -> 4
+// MISO 18
+// MOSI 19
+// SS 23
+
+/// @brief configuration of CC1200 sub 1-GHz radio transceiver
+typedef struct {
+    SPIClass *spi;
+    int8_t sck = 5;
+} cc1200_config_t;
+
+// @brief CC1200 Command Strobes (0x30 ≤ SPI Address Space ≤ 0x3D)
+typedef enum {
+    SRES = 0x30, // reset chip
+    SFSTXON, // enable and calibrate FS if SETTLING_CFG.FS_AUTOCAL=1
+    // SFSTXON wait FS for quick RX/TX when from RX & PKT_CFG2.CCA_MODE ≠ 0
+    SXOFF,   // enter XOFF state when CSn is de-asserted
+    SCAL,    // calibrate FS and turn it off
+    // SRX/STX perform SFSTXON first from IDLE if SETTLING_CFG.FS_AUTOCAL=1 
+    SRX,     // enable RX
+    STX,     // enable TX from IDLE.
+    // STX only to TX if clear channel when RX & PKT_CFG2.CCA_MODE ≠ 0 
+    SIDLE,   // exit RX/TX, turn off FS and exit eWOR mode
+    SAFC,    // Automatic Frequency Compensation
+    SWOR,    // start eWOR (Section 9.6) if WOR_CFG0.RC_PD=0
+    SPWD,    // enter SLEEP mode when CSn is de-asserted
+    SFRX,    // flush the RX FIFO. Only issue SFRX in IDLE or RX_FIFO_ERR states    
+    SFTX,    // flush the TX FIFO. Only issue SFTX in IDLE or TX_FIFO_ERR states
+    SWORRST, // reset the eWOR timer to the Event1 value
+    SNOP     // no operation. May be used to get access to the chip status byte
+} cc1200_command_strobe_t;
+
+int cc1200_init(cc1200_config_t cc1200);
