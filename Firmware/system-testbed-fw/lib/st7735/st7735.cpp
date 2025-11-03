0ed31749a39e53f87f35e09495ffc76f0d4515c0
ericmutton <ericmutton@protonmail.com>
2025-11-02 22:19:46 -0600

    feat: draft simplified display driver #86

---
+++ Firmware/system-testbed-fw/lib/st7735/st7735.cpp
@@ -0,0 +1,4 @@
+#include <st7735.h>
+
+extern Adafruit_ST7789 tft;
+
---
+++ Firmware/system-testbed-fw/lib/st7735/st7735.h
@@ -0,0 +1,8 @@
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Fonts/FreeMono9pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBoldOblique9pt7b.h>
+#include <Fonts/FreeSerifBoldItalic12pt7b.h>
