f651893afd4c2e5972bcc5f214dc050e90be9b67
ericmutton <ericmutton@protonmail.com>
2025-11-06 21:09:46 -0600

    feat: move I2C scan debugging into port expander initialization

--- Firmware/system-testbed-fw/lib/pcf8575/pcf8575.cpp
+++ Firmware/system-testbed-fw/lib/pcf8575/pcf8575.cpp
@@ -19,5 +19,23 @@ int pcf8575_init(Adafruit_PCF8575 &pcf, pcf8575_config_t &configuration) {
   pcf.begin(configuration.sensor_address, configuration.i2c);
   pinMode(configuration.pin_interrupt, INPUT_PULLUP);
   // attachInterrupt(digitalPinToInterrupt(configuration.pin_interrupt), portChanged, FALLING);
+  #ifdef DEBUG_PCF8575
+  DEBUG_PCF8575.println("\nStarting I2C scan...");
+  #endif
+  bool found = false;
+  for (uint8_t addr = 1; addr < 127; addr++) { // Iterates through all addresses 1-126
+      Wire.beginTransmission(addr);            // If address is found this should begin the exchange with said address
+      if (Wire.endTransmission() == 0) {       // ends that exchange
+          #ifdef DEBUG_PCF8575
+          DEBUG_PCF8575.print("Found device at 0x");
+          DEBUG_PCF8575.println(addr, HEX);
+          #endif
+          found = true;
+      }
+  }
+  #ifdef DEBUG_PCF8575
+  if (!found) DEBUG_PCF8575.println("No devices found.");
+  #endif
+
   return 0;
 }
